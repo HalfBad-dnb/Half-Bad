2f3948a5c57f7eb5ac9a0f5ebe532aa8ba9f09bf
Deividas Stropus <dstropus@icloud.com>
2025-02-15 07:45:09 +0200

    update

--- Pirk/src/main/java/com/Pirk/Pirk/Controllers/AdminPanelController.java
+++ Pirk/src/main/java/com/Pirk/Pirk/Controllers/AdminPanelController.java
@@ -6,11 +6,8 @@ import com.Pirk.Pirk.models.Order;
 import com.Pirk.Pirk.services.AdminService;
 import com.Pirk.Pirk.models.AdminPanel;
 import org.springframework.http.ResponseEntity;
-
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
